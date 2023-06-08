235eb932386914bc109143ca447f2ee19ba29b32
demix007 <ayodejiabidemi007@gmail.com>
2023-06-08 22:20:25 +0100

    fix eslint errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,29 +2,27 @@ import React from 'react';
 import './Hero.css';
 import CRYPTO from '../assets/hero-img.png';
 
-export const Hero = () => {
-  return (
-    <div className="hero">
-      <div className="container">
-        
-        <div className="left">
-          <p>Buy & Sell Crypto 24/7</p>
-          <h1>Invest in Cryptocurrency with your IRA</h1>
-          <p>Buy, Sell, and store hundreds of cryptocurrencies</p>
-          <div className="input-container">
-            <input type="email" name="email" placeholder="Enter Your Email" />
-            <button className='btn'>Learn More</button>
-          </div>
-        </div>
+export const Hero = () => (
+  <div className="hero">
+    <div className="container">
 
-        <div className="right">
-            <div className="img-container">
-                <img src={CRYPTO} alt="crypto" />
-            </div>
+      <div className="left">
+        <p>Buy & Sell Crypto 24/7</p>
+        <h1>Invest in Cryptocurrency with your IRA</h1>
+        <p>Buy, Sell, and store hundreds of cryptocurrencies</p>
+        <div className="input-container">
+          <input type="email" name="email" placeholder="Enter Your Email" />
+          <button className="btn" type="button">Learn More</button>
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="img-container">
+          <img src={CRYPTO} alt="crypto" />
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+);
 
 export default Hero;
