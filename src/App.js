cf0539f246bbc4e07bd41c5003a28a799c048496
demix007 <ayodejiabidemi007@gmail.com>
2023-06-08 21:09:31 +0100

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <Navbar />
-)
+);
 
-export default App
+export default App;
