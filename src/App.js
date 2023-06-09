6941261b0c63f1a681589edab9924300a684b1bf
demix007 <ayodejiabidemi007@gmail.com>
2023-06-09 19:04:42 +0100

    Add register route to the app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Featured from './components/featured/Featured';
+import Register from './components/register/Register';
 
 const App = () => (
   <>
     <Navbar />
     <Hero />
     <Featured />
+    <Register />
   </>
 );
 
