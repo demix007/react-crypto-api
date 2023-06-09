17ebaa2fc0e45b9bcde2b78958621c29337c4227
demix007 <ayodejiabidemi007@gmail.com>
2023-06-09 06:38:53 +0100

    fetch data from the API

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './Featured.css';
 import BTC from '../../assets/btc-img.png';
 import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi';
 
 const Featured = () => {
+  const [data, setData] = useState([]);
 
+  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en"
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setData(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  console.log(data)
   return (
     <div className="featured">
       <div className="container">
